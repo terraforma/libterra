021668b7b50d698e058b139bcdef7041f435bec5
Tom Gascoigne <tom@gascoigne.me>
2013-01-17 12:56:58 +0000

    Move the tfVersion definition into the namespace
    This isn't necessary, I just thought it made sense since we already have
    the GetVersion prototype in there.

--- include/version.h
+++ include/version.h
@@ -14,30 +14,30 @@ namespace LibTerra
 	============================================================
 	*/
 	tfVersion GetVersion();
-};
 
-/*
-================================================================
-	Version
+	/*
+	================================================================
+		Version
 
-	Encapsulates the versioning system and provides access to
-	the major, minor, and patch versions.
-================================================================
-*/
-class LibTerra::tfVersion
-{
-	friend tfVersion GetVersion();
-private:
-	const unsigned int m_major;
-	const unsigned int m_minor;
-	const unsigned int m_patch;
+		Encapsulates the versioning system and provides access to
+		the major, minor, and patch versions.
+	================================================================
+	*/
+	class tfVersion
+	{
+		friend tfVersion GetVersion();
+	private:
+		const unsigned int m_major;
+		const unsigned int m_minor;
+		const unsigned int m_patch;
 
-	tfVersion(const unsigned int _major, const unsigned int _minor, const unsigned int _patch) :
-		m_major(_major), m_minor(_minor), m_patch(_patch) {}
-public:
-	const unsigned int Major() const { return m_major; }
-	const unsigned int Minor() const { return m_minor; }
-	const unsigned int Patch() const { return m_patch; }
+		tfVersion(const unsigned int _major, const unsigned int _minor, const unsigned int _patch) :
+			m_major(_major), m_minor(_minor), m_patch(_patch) {}
+	public:
+		const unsigned int Major() const { return m_major; }
+		const unsigned int Minor() const { return m_minor; }
+		const unsigned int Patch() const { return m_patch; }
+	};
 };
 
 #endif /* _LIBTERRA_INCLUDE_VERSION_H_ */
