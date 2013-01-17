b0a4924e4eb4b1c20052afde785eabd53d69c1c1
Tom Gascoigne <tom@gascoigne.me>
2013-01-17 10:45:04 +0000

    Refactor version.{h,cpp}
    Provides a more logical interface to the version number.

--- include/version.h
+++ include/version.h
@@ -3,15 +3,25 @@
 
 namespace LibTerra
 {
-	class SemVer;
-	
-	SemVer GetVersion();
+	class Version;
+
+	Version GetVersion();
 };
 
-class LibTerra::SemVer
+class LibTerra::Version
 {
-public :
-	unsigned m_major, m_minor, m_patch;
+	friend Version GetVersion();
+private:
+	const unsigned int m_major;
+	const unsigned int m_minor;
+	const unsigned int m_patch;
+
+	Version(const unsigned int _major, const unsigned int _minor, const unsigned int _patch) :
+		m_major(_major), m_minor(_minor), m_patch(_patch) {}
+public:
+	const unsigned int Major() const { return m_major; }
+	const unsigned int Minor() const { return m_minor; }
+	const unsigned int Patch() const { return m_patch; }
 };
 
 # endif /* __LIBTERRA_INCLUDE_VERSION_H_ */
