01dc44923d73a0c72e491d691ccfc04c4d45d29a
Tom Gascoigne <tom@gascoigne.me>
2013-04-06 23:25:35 +0100

    Add a Vec3fAdd function

--- include/libterra/Vector.h
+++ include/libterra/Vector.h
@@ -8,6 +8,7 @@ namespace LibTerra {
 		float z;
 	};
 	tfVec3f MakeVec3f(float x, float y, float z);
+	tfVec3f Vec3fAdd(tfVec3f v, tfVec3f w);
 }
 
 #endif /* _INCLUDE_LIBTERRA_VECTOR_H_ */
--- src/Vector.cpp
+++ src/Vector.cpp
@@ -6,4 +6,10 @@ namespace LibTerra {
 		tfVec3f vec = {x, y, z};
 		return vec;
 	}
+
+	tfVec3f Vec3fAdd(tfVec3f v, tfVec3f w)
+	{
+		tfVec3f vec = {v.x+w.x, v.y+w.y, v.z+w.z};
+		return vec;
+	}
 }
